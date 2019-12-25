c3f2816ab319b5343ce8335ec88cfc55f36fe2ad
sameenzjt <sameen.zjt@gmail.com>
2019-12-25 15:50:54 +0800

    v0.11.2文章页显示逻辑优化，增加CDN引用font awesome，引用iconfont图标

--- header.php
+++ header.php
@@ -67,7 +67,8 @@
     <link rel="stylesheet" type="text/css" href="https://cdn.staticfile.org/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
     
-    
+    <link rel="stylesheet" type="text/css" href="https://cdn.staticfile.org/font-awesome/5.12.0-1/css/all.min.css" type="text/css"/>
+    <link rel="stylesheet" type="text/css" href="https://at.alicdn.com/t/font_1581944_irp6dkc79dt.css" type="text/css"/>
     <!-- 自定义style样式 -->
     <?php $header_style = of_get_option('header_style');
         if(empty($header_style)){
--- single.php
+++ single.php
@@ -1,21 +1,22 @@
 <?php get_header(); ?>
 
-        <div class="row">
-            <div class="col-12">
-                <?php if(function_exists('cmp_breadcrumbs')) cmp_breadcrumbs();?>
+<?php $article_type = get_field('article-type');
+    $reprinted_from = get_field('reprinted-from');
+    $reprinted_url = get_field('reprinted-url');
+    $translation_from = get_field('translation-from');
+    $translation_url = get_field('translation-url'); 
+?>
 
-                <h1><?php the_title(); ?></h1>
-                
+<div class="row">
+    <div class="col-12">
+        <?php if(function_exists('cmp_breadcrumbs')) cmp_breadcrumbs();?>
 
-                <p class="font-size-small-14 post-info">
+        <h1 class="post-title"><?php the_title(); ?></h1>
+        
 
+        <p class="font-size-small-14 post-info">
+            <span>
                 <?php
-                    $article_type = get_field('article-type');
-                    $reprinted_from = get_field('reprinted-from');
-                    $reprinted_url = get_field('reprinted-url');
-                    $translation_from = get_field('translation-from');
-                    $translation_url = get_field('translation-url');
-
                     if($article_type == "original") {
                         echo "<span class='badge badge-info'>原创</span>";
                         echo "<span>作者：Lyudmil</span>";
@@ -27,112 +28,122 @@
                         echo "<span>原文来源：" . $translation_from . "</span>";
                     }
                 ?>
-                    
-                    <span><?php the_time('Y年n月j日') ?></span>
-                    <span>阅读 <?php get_post_views($post -> ID); ?></span>
-                    <span>点赞 82</span>
-                    <span><?php comments_popup_link('0 条评论', '1 条评论', '% 条评论', '', '评论已关闭'); ?></span>
-
-                    <span><?php edit_post_link('编辑', ' • ', ''); ?></span>
-                </p>
-                <div class="dropdown-divider"></div>
-            </div>
+            </span>
+            <span><?php the_time('Y年n月j日') ?></span>
+            <span>阅读 <?php get_post_views($post -> ID); ?></span>
+            <span><?php comments_popup_link('评论 0', '评论 1', '评论 %', '', ''); ?></span>
+            <span><?php edit_post_link('编辑', '[', ']'); ?></span>
+        </p>
+        <div class="dropdown-divider"></div>
+    </div>
 
 
-            <div class="col-lg-9 col-sm-12 container">
-            <?php if (have_posts()) : the_post(); update_post_caches($posts); ?>
-                <!--文章内容-->
-                <div class="post-content">
-                    <?php the_content(); ?>
+    <div class="col-lg-9 col-sm-12 container">
+    <?php if (have_posts()) : the_post(); update_post_caches($posts); ?>
+        <!--文章内容-->
+        <div class="post-content">
+            <?php the_content(); ?>
 
-                    <?php the_field('001'); ?>
-
-                    
-                    
+            
+            
 
 
-
-                </div>
-                <?php else : ?>
-                <div class="errorbox">
-                    没有文章！
-                </div>
-                <?php endif; ?>
-
-                <!-- 二维码引流 -->
-                <div class="post-qr" style="background:red;">
-                    <p>扫码关注公众号</p>
-                    <img src="<?php echo of_get_option('weixin_qr_uploader', ''); ?>" width="120px">
-                </div>
-                <!-- 版权声明 -->
-                <div class="post-copyright-notice">
-                    <div class="dropdown-divider"></div>
-                    <p>文章为作者独立观点不代表优设网立场，未经允许不得转载。</p>
-                    <div class="dropdown-divider"></div>
-                </div>
-                <!-- 文章标签 -->
-                <div class="post-tags">
-                    <p>继续阅读与本文标签相同的文章</p>
-                    <?php the_tags('<span class="post-tags-badge">','</span><span class="post-tags-badge">','</span>'); ?>
-                </div>
-                <!-- 评论 -->
-                <div style="margin: 40px 0; padding: 0px 90px 0px 100px;">
-                    <?php comments_template(); ?>
-                </div>
-
-            </div>
-
-            <!-- 右 -->
-            <div class="col-lg-3 hide-768px">
-                <div class="single-tool" id="fixed-tool">
-                    <h5 class="lanmu-title">文章小工具</h5>
-                    <div class="dropdown-divider"></div>
-                    <p class="font-size-small-14">《<?php the_title(); ?>》</p>
-                    <img src="<?php bloginfo('template_url'); ?>/images/time.png" style="float: left; margin: 4px 6px 0px 0px ;">
-                    <p class="font-size-small-14"><?php echo count_words_read_time(); ?></p>
-                    <div class="post-font-size-change">
-                        <a href="javascript:void(0);" class="font-size-small-14"><span class="smaller">A-</span></a>
-                        <a href="javascript:void(0);" class="font-size-small-14"><span>14</span></a>
-                        <a href="javascript:void(0);" class="font-size-small-14"><span>16</span></a>
-                        <a href="javascript:void(0);" class="font-size-small-14"><span>18</span></a>
-                        <a href="javascript:void(0);" class="font-size-small-14"><span>20</span></a>
-                        <a href="javascript:void(0);" class="font-size-small-14"><span class="bigger">A+</span></a>
-                    </div>
-                </div>
-                <div class="single-tool">
-                    <h5 class="lanmu-title">相似文章</h5>
-                    <div class="dropdown-divider"></div>
-                    <ul style="list-style-type:none; margin: 0px; padding: 0px; font-size: 14px;">
-                        <?php $i=1;
-                            $cats = wp_get_post_categories($post->ID);
-                            if ($cats) {
-                                $args = array(
-                                'category__in' => array( $cats[0] ),                   
-                                'showposts' => 10,
-                                'ignore_sticky_posts' => 1
-                                );
-                            query_posts($args);
-                            if (have_posts()) :
-                                while (have_posts()) : the_post(); update_post_caches($posts); ?>
-                                    <li style="list-style-type: none; display: block;">
-                                        <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-                                            <span class="num"><?php echo $i;$i++; ?></span>
-                                            <?php the_title(); ?>
-                                        </a>
-                                    </li>
-                            <?php endwhile; else : ?>
-                                <li> 暂无文章</li>
-                            <?php endif; wp_reset_query(); } ?>                                                        
-                    </ul>
-                </div>
-
-
-
-
-                
-            </div>
 
         </div>
+        <?php else : ?>
+        <div class="errorbox">
+            没有文章！
+        </div>
+        <?php endif; ?>
+
+        <!-- 二维码引流 -->
+        <div class="post-qr" style="background:red;">
+            <p>扫码关注公众号</p>
+            <img src="<?php echo of_get_option('weixin_qr_uploader', ''); ?>" width="120px">
+        </div>
+        <!-- 版权声明 -->
+        <div class="post-copyright-notice">
+            <div class="dropdown-divider"></div>
+                <?php
+                    if($article_type == "original") {
+                        $the_permalink = get_permalink();
+                        $the_title = get_the_title();
+                        echo "转载请注明出处：<a href='" . $the_permalink ."'>《".$the_title."》</a>";
+                        echo "<a href='javascript:void(0)' onclick='copy_code('" .$the_permalink. "'); return false;' class='click-copy-link'><i class='iconfont icon-link' aria-hidden='true'></i>点击复制链接</a>";
+                        echo "<br />文章为作者独立观点不代表本网站立场";
+
+                    } elseif ($article_type == "reproduced") {
+                        echo "<p>转载自：<a href='" . $reprinted_url ."'>" . $reprinted_from . "</a></p>";
+                        echo "<p>本文转载自其他网站，请勿再次转载本文</p>";
+                    } elseif ($article_type == "translation") {
+                        echo "<p>原文：<a href='" . $translation_url ."'>" . $translation_from . "</a></p>";
+                        echo "<p>本文翻译自其他文章，可能存在翻译错误，本网站不保证文章准确性</p>";
+                }?>
+            <div class="dropdown-divider"></div>
+        </div>
+        <!-- 文章标签 -->
+        <div class="post-tags">
+            <p>继续阅读与本文标签相同的文章</p>
+            <?php the_tags('<span class="post-tags-badge">','</span><span class="post-tags-badge">','</span>'); ?>
+        </div>
+        <!-- 评论 -->
+        <div style="margin: 40px 0; padding: 0px 90px 0px 100px;">
+            <?php comments_template(); ?>
+        </div>
+
+    </div>
+
+    <!-- 右 -->
+    <div class="col-lg-3 hide-768px">
+        <div class="single-tool" id="fixed-tool">
+            <h5 class="lanmu-title">文章小工具</h5>
+            <div class="dropdown-divider"></div>
+            <p class="font-size-small-14">《<?php the_title(); ?>》</p>
+            <img src="<?php bloginfo('template_url'); ?>/images/time.png" style="float: left; margin: 4px 6px 0px 0px ;">
+            <p class="font-size-small-14"><?php echo count_words_read_time(); ?></p>
+            <div class="post-font-size-change">
+                <a href="javascript:void(0);" class="font-size-small-14"><span class="smaller">A-</span></a>
+                <a href="javascript:void(0);" class="font-size-small-14"><span>14</span></a>
+                <a href="javascript:void(0);" class="font-size-small-14"><span>16</span></a>
+                <a href="javascript:void(0);" class="font-size-small-14"><span>18</span></a>
+                <a href="javascript:void(0);" class="font-size-small-14"><span>20</span></a>
+                <a href="javascript:void(0);" class="font-size-small-14"><span class="bigger">A+</span></a>
+            </div>
+        </div>
+        <div class="single-tool">
+            <h5 class="lanmu-title">相似文章</h5>
+            <div class="dropdown-divider"></div>
+            <ul style="list-style-type:none; margin: 0px; padding: 0px; font-size: 14px;">
+                <?php $i=1;
+                    $cats = wp_get_post_categories($post->ID);
+                    if ($cats) {
+                        $args = array(
+                        'category__in' => array( $cats[0] ),                   
+                        'showposts' => 10,
+                        'ignore_sticky_posts' => 1
+                        );
+                    query_posts($args);
+                    if (have_posts()) :
+                        while (have_posts()) : the_post(); update_post_caches($posts); ?>
+                            <li style="list-style-type: none; display: block;">
+                                <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+                                    <span class="num"><?php echo $i;$i++; ?></span>
+                                    <?php the_title(); ?>
+                                </a>
+                            </li>
+                    <?php endwhile; else : ?>
+                        <li> 暂无文章</li>
+                    <?php endif; wp_reset_query(); } ?>                                                        
+            </ul>
+        </div>
+
+
+
+
+        
+    </div>
+
+</div>
     
 
 <?php get_footer(); ?>
