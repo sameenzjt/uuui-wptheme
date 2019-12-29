c75e6e89d22ce1dd279e39c9b8507987bf45bded
sameenzjt <sameen.zjt@gmail.com>
2019-12-29 19:05:39 +0800

    v0.13.1：网站mate标签改进

--- header.php
+++ header.php
@@ -19,30 +19,39 @@
                 wp_title('',true);
             } ?>
     </title>
-    <?php
-        $description = '';
-        $keywords = '';
+    <?php $description = ''; $keywords = '';
 
-        if (is_home() || is_page()) {
-        // 将以下引号中的内容改成你的主页description
+        if (is_home()) {
+            // 将以下引号中的内容改成你的主页description
             $description = of_get_option('website_description', '');
             // 将以下引号中的内容改成你的主页keywords
             $keywords = of_get_option('website-keywords', '');
+
+            $title = get_bloginfo('name');
+        }elseif (is_page()){
+            // 将以下引号中的内容改成你的主页description
+            $description = of_get_option('website_description', '');
+            // 将以下引号中的内容改成你的主页keywords
+            $keywords = of_get_option('website-keywords', '');
+
+            $title = get_bloginfo('name')."-".get_the_title();
         }elseif (is_single()) {
+            $title = get_the_title()."-".get_bloginfo('name');
             $description1 = get_post_meta($post->ID, "description", true);
             $description2 = str_replace("\n","",mb_strimwidth(strip_tags($post->post_content), 0, 200, "…", 'utf-8'));
             // 填写自定义字段description时显示自定义字段的内容，否则使用文章内容前200字作为描述
             $description = $description1 ? $description1 : $description2;
             // 填写自定义字段keywords时显示自定义字段的内容，否则使用文章tags作为关键词
             $keywords = get_post_meta($post->ID, "keywords", true);
-        if($keywords == '') {
-            $tags = wp_get_post_tags($post->ID);
-            foreach ($tags as $tag ) {
-                $keywords = $keywords . $tag->name . ", ";
-            }
-            $keywords = rtrim($keywords, ', ');
+            if($keywords == '') {
+                $tags = wp_get_post_tags($post->ID);
+                foreach ($tags as $tag ) {
+                    $keywords = $keywords . $tag->name . ", ";
+                }
+                $keywords = rtrim($keywords, ', ');
         }
         }elseif (is_category()) {
+            $title = get_bloginfo('name')."-".single_cat_title('', false);
             // 分类的description可以到后台 - 文章 -分类目录，修改分类的描述
             $description = category_description();
             $keywords = single_cat_title('', false);
@@ -50,13 +59,17 @@
             // 标签的description可以到后台 - 文章 - 标签，修改标签的描述
             $description = tag_description();
             $keywords = single_tag_title('', false);
-            }
-            $description = trim(strip_tags($description));
-            $keywords = trim(strip_tags($keywords));
-        ?>
+            $title = get_bloginfo('name')."-".single_tag_title('', false);
+        }
+        $description = trim(strip_tags($description));
+        $keywords = trim(strip_tags($keywords));
+        $title = trim(strip_tags($title));
+    ?>
+
+
+    <meta name="title" content="<?php echo $title; ?>">
     <meta name="description" content="<?php echo $description; ?>" />
     <meta name="keywords" content="<?php echo $keywords; ?>" />
-    
 
 
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
