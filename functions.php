ee0232f985e214d7421efeee664f33e157bdf49a
sameenzjt <sameen.zjt@gmail.com>
2020-01-02 23:47:23 +0800

    v0.16.2：文章编辑页将作者模块移到发布模块内

--- functions.php
+++ functions.php
@@ -360,7 +360,6 @@ function pagination($query_string){
 /* —— 文章列表分页 —— 结束 */
 
 
-
 /**显示页面查询次数、加载时间和内存占用
  * 默认在页脚显示 From wpdaxue.com
  * 调用：if(function_exists('performance')) performance(false) ;
@@ -376,5 +375,30 @@ function performance( $visible = false ) {
 add_action( 'wp_footer', 'performance', 20 );
 
 /**显示页面查询次数、加载时间和内存占用 —— 结束*/
+
+
+/** —— 文章编辑页将作者模块移到发布模块内 ——*/
+add_action( 'admin_menu', 'remove_author_metabox' );
+add_action( 'post_submitbox_misc_actions', 'move_author_to_publish_metabox' );
+function remove_author_metabox() {
+    remove_meta_box( 'authordiv', 'post', 'normal' );
+}
+function move_author_to_publish_metabox() {
+    global $post_ID;
+    $post = get_post( $post_ID );
+    echo '<div id="author" class="misc-pub-section" style="border-top-style:solid; border-top-width:1px; border-top-color:#EEEEEE; border-bottom-width:0px;">作者： ';
+    post_author_meta_box( $post );
+    echo '</div>';
+}
+/** —— 文章编辑页将作者模块移到发布模块内 —— 结束*/
+
+
+
+
+
+
+
+
+
 
 ?>
